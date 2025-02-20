ae823489c4d4b4259210ac76689a863e8a8fd05b
harry <hk.mca08@gmail.com>
2025-02-20 08:44:35 +0530

    update/test_case

--- lib/jio_webview_platform_interface.dart
+++ lib/jio_webview_platform_interface.dart
@@ -2,3 +2,5 @@ library jio_webview_platform_interface;
 
 export 'package:jio_webview_platform_interface/plugin/jio_plugin_platform.dart';
 export 'package:jio_webview_platform_interface/webview/jio_webview_platform.dart';
+export 'package:jio_webview_platform_interface/webview/webview_platform_interface.dart';
+
---
+++ test/webview_test/fake_webview_platform.dart
@@ -0,0 +1,79 @@
+// test/fake_webview_platform.dart
+
+import 'package:flutter/services.dart';
+import 'package:jio_webview_platform_interface/webview/webview_platform_interface.dart';
+
+/// A fake implementation of [WebviewPlatformInterface] for testing.
+class FakeWebviewPlatform extends WebviewPlatformInterface {
+  FakeWebviewPlatform({super.customMethodChannel});
+
+  @override
+  MethodChannel getWebviewMethodChannel() {
+    return methodChannel;
+  }
+
+  @override
+  Future<void> reload() async {
+    // Simply complete.
+  }
+
+  @override
+  Future<bool> canGoBack() async {
+    return true;
+  }
+
+  @override
+  Future<void> goBack() async {
+    // Simply complete.
+  }
+
+  @override
+  Future<String> getCurrentUrl() async {
+    return 'https://example.com';
+  }
+
+  @override
+  Future<void> loadUrl(String url) async {
+    // For testing, we do nothing or you could record the url if needed.
+  }
+
+  @override
+  Future<void> loadHtmlAsset(String assetPath) async {
+    // Do nothing.
+  }
+
+  @override
+  Future<void> loadHtmlString(String htmlString) async {
+    // Do nothing.
+  }
+
+  @override
+  Future<void> clearCache() async {
+    // Do nothing.
+  }
+
+  @override
+  Future<void> clearLocalStorage() async {
+    // Do nothing.
+  }
+
+  @override
+  Future<String> getUserAgent() async {
+    return 'fakeUserAgent';
+  }
+
+  @override
+  Future<void> setUserAgent(String userAgent) async {
+    // Do nothing.
+  }
+
+  @override
+  Future<String> evaluateJavascript(String script) async {
+    return 'evaluated:$script';
+  }
+
+  @override
+  Future<String> runJavaScript(String script) async {
+    return 'ran:$script';
+  }
+}
